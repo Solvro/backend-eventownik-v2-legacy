6f71e5a13067b687bbf376ab322c2a6cbc38d33e
ibtyog <piotrhirkyj@gmail.com>
2024-12-08 16:55:26 +0100

    feat: participants migration

---
+++ database/migrations/1733574513841_create_participants_table.ts
@@ -0,0 +1,21 @@
+import { BaseSchema } from '@adonisjs/lucid/schema'
+
+export default class extends BaseSchema {
+  protected tableName = 'participants'
+
+  async up() {
+    this.schema.createTable(this.tableName, (table) => {
+      table.increments('id')
+      table.string('email', 255).notNullable()
+      table.integer('event_id').unsigned().nullable().references('id').inTable('events').onDelete('CASCADE')
+      table.string('first_name', 255).notNullable()
+      table.string('last_name', 255).notNullable()
+      table.timestamp('created_at')
+      table.timestamp('updated_at')
+    })
+  }
+
+  async down() {
+    this.schema.dropTable(this.tableName)
+  }
+}
