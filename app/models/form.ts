eaab3dd79b2a9f1ba66868baccf8248b9d51b9e1
Skiperpol <skiperpol1@gmail.com>
2024-12-13 14:07:20 +0100

    feat: add Form model

---
+++ app/models/form.ts
@@ -0,0 +1,35 @@
+import { DateTime } from 'luxon'
+import { BaseModel, column, belongsTo, BelongsTo } from '@adonisjs/lucid/orm';
+// import Event from './event.ts';
+
+export default class Form extends BaseModel {
+  @column({ isPrimary: true })
+  declare id: number;
+
+  @column()
+  declare eventId: number;
+
+  @column()
+  declare isOpen: boolean;
+
+  @column()
+  declare description: string;
+
+  @column()
+  declare name: string;
+
+  @column.dateTime({ autoCreate: true })
+  declare startDate: DateTime;
+
+  @column.dateTime()
+  declare endDate: DateTime | null;
+
+  @column.dateTime({ autoCreate: true })
+  declare createdAt: DateTime;
+
+  @column.dateTime({ autoCreate: true, autoUpdate: true })
+  declare updatedAt: DateTime;
+
+  // @belongsTo(() => Event)
+  // public event: BelongsTo<typeof Event>;
+}
