17e771ac276f85b2fe6efd3c4b10b672a9a1cf7c
ibtyog <piotrhirkyj@gmail.com>
2024-12-13 22:35:08 +0100

    participants model add

---
+++ app/models/participant.ts
@@ -0,0 +1,25 @@
+import { DateTime } from 'luxon'
+import { BaseModel, column } from '@adonisjs/lucid/orm'
+
+export default class Participant extends BaseModel {
+  @column({ isPrimary: true })
+  declare id: number
+
+  @column()
+  declare email: string
+
+  @column()
+  declare eventId: number
+
+  @column()
+  declare firstName: string
+
+  @column()
+  declare lastName: string
+
+  @column.dateTime({ autoCreate: true })
+  declare createdAt: DateTime
+
+  @column.dateTime({ autoCreate: true, autoUpdate: true })
+  declare updatedAt: DateTime
+}
