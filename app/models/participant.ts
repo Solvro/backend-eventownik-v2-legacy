a988b6f4e0c3cc937deb18313936da2dc6d37240
Dawid Linek <dawid@linek.dev>
2024-12-15 21:25:13 +0100

    feat: add event relation to participant

--- app/models/participant.ts
+++ app/models/participant.ts
@@ -1,29 +1,34 @@
-import { DateTime } from 'luxon'
-import { BaseModel, column, hasMany } from '@adonisjs/lucid/orm'
+import { BaseModel, belongsTo, column, hasMany } from "@adonisjs/lucid/orm";
+import type { BelongsTo } from "@adonisjs/lucid/types/relations";
+import { DateTime } from "luxon";
+import Event from '#models/event'
 // import ParticipantAttribute from 'App/Models/participantAttribute.ts'
 
 export default class Participant extends BaseModel {
   @column({ isPrimary: true })
-  declare id: number
+  declare id: number;
 
   @column()
-  declare email: string
+  declare email: string;
 
   @column()
-  declare eventId: number
+  declare eventId: number;
 
   @column()
-  declare firstName: string
+  declare firstName: string;
 
   @column()
-  declare lastName: string
+  declare lastName: string;
 
   @column.dateTime({ autoCreate: true })
-  declare createdAt: DateTime
+  declare createdAt: DateTime;
 
   @column.dateTime({ autoCreate: true, autoUpdate: true })
-  declare updatedAt: DateTime
+  declare updatedAt: DateTime;
 
   // @hasMany(() => ParticipantAttribute)
   // declare attributes: HasMany<typeof ParticipantAttribute>
+
+  @belongsTo(() => Event)
+  declare event: BelongsTo<typeof Event>;
 }
