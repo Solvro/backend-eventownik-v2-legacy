8cb563f1bc2659979c6e05b7c70c7699c2c1e617
GOLDER303 <szymonstepien@interia.pl>
2024-12-18 20:23:37 +0100

    feat: connect admin and permission models with a relation

--- app/models/permission.ts
+++ app/models/permission.ts
@@ -1,4 +1,7 @@
-import { BaseModel, column } from "@adonisjs/lucid/orm";
+import { BaseModel, column, manyToMany } from "@adonisjs/lucid/orm";
+import type { ManyToMany } from "@adonisjs/lucid/types/relations";
+
+import Admin from "./admin.js";
 
 export default class Permission extends BaseModel {
   @column({ isPrimary: true })
@@ -9,4 +12,10 @@ export default class Permission extends BaseModel {
 
   @column()
   declare subject: string;
+
+  @manyToMany(() => Admin, {
+    pivotTable: "admin_permissions",
+    pivotColumns: ["eventId"],
+  })
+  declare admins: ManyToMany<typeof Admin>;
 }
