715c91e6c405ba246aac7d7a6717b87b40d808aa
ibtyog <piotrhirkyj@gmail.com>
2025-01-02 09:45:51 +0100

    routes add

--- start/routes.ts
+++ start/routes.ts
@@ -7,9 +7,19 @@
 |
 */
 import router from "@adonisjs/core/services/router";
-
+import("#controllers/participants_controller");
+import ParticipantsController from "#controllers/participants_controller";
+const participants = new ParticipantsController();
 router.get("/", async () => {
   return {
     hello: "world",
   };
 });
+
+router.get("/api/v1/participants",participants.index);
+router.get("/api/v1/participants/:id",participants.show);
+
+router.post("/api/v1/participants",participants.store);
+router.patch("/api/v1/participants/:id",participants.edit);
+
+router.delete("/api/v1/participants/:id",participants.destroy);
