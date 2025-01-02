b6eb4d806db0bf12cf348e487674fb4e59b4577d
ibtyog <piotrhirkyj@gmail.com>
2025-01-02 19:26:33 +0100

    participants controller validator add

--- app/controllers/participants_controller.ts
+++ app/controllers/participants_controller.ts
@@ -1,5 +1,6 @@
 import { HttpContext } from '@adonisjs/core/http'
 import Participant from '#models/participant'
+import { participantsStoreValidator, participantsUpdateValidator } from '#validators/participants'
 
 export default class ParticipantsController {
   /**
@@ -7,16 +8,11 @@ export default class ParticipantsController {
    */
 
   async index({response}: HttpContext) {
-    try{
-      const participants = await Participant.all()
+      const participants = await Participant.all();
       if (!participants) {
-        return response.status(404).send({message: 'No participants found'})
+        return response.status(404).send([]);
       }
-      return response.status(200).send(participants)
-    } catch (error) {
-      console.error(error)
-      return response.status(500).send({message: 'Internal server error'})
-  }
+      return participants;
 }
 
   /**
@@ -24,75 +20,35 @@ export default class ParticipantsController {
    */
 
   async store({ request, response }: HttpContext) {
-    // Walidacja danych, do wrzucenia jako middleware
-    if (request.input('email') == null || request.input('eventId') == null || request.input('firstName') == null || request.input('lastName') == null) {
-      console.log('Invalid request')
-      return response.status(400).send({message: 'Invalid request'})
-    }  
-    // Zapisanie danych do bazy
-    try {
-      const data = request.only(['email', 'eventId', 'firstName', 'lastName'])
-      const participant = await Participant.create(data)
-      return response.status(201).send(participant)
-    // Błąd niewynikający z błędu użytkownika
-    }catch (error) {
-      console.error(error)
-      return response.status(500).send({message: 'Internal server error'})
-  }
-
+    const data = await participantsStoreValidator.validate(request.all());
+    const participant = await Participant.create(data);
+    return response.status(201).send(participant);
   }
 
   /**
    * Show individual record
    */
-  async show({ params, response }: HttpContext) {
-    try {
-      const participant = await Participant.find(params.id)
-      if (!participant) {
-        return response.status(404).send({message: `Participant with ID ${params.id} was not found`})
-      }
-      return response.status(200).send(participant)
-    } catch (error) {
-      console.error(error)
-      return response.status(500).send({message: 'Internal server error'})
-  }
+  async show({ params }: HttpContext) {
+      return await Participant.findOrFail(params.id);
 }
 
   /**
    * Edit individual record
    */
-  async edit({ params, request, response }: HttpContext) {
-    try {
-      const participant = await Participant.find(params.id)
-      if (!participant) {
-        return response.status(404).send({message: `Participant with ID ${params.id} was not found`})
-      }
-      const data = request.only(['email', 'eventId', 'firstName', 'lastName'])
-      participant.merge(data)
-      await participant.save()
-      return response.status(200).send(participant)
-    }
-    catch (error) {
-      console.error(error)
-      return response.status(500).send({message: 'Internal server error'})
-    }
-
+  async edit({ params, request }: HttpContext) {
+    const data = await participantsUpdateValidator.validate(request.all());
+    const participant = await Participant.findOrFail(params.id);
+    participant.merge(data);
+    await participant.save();
+    return {"message": `Participant successfully updated.`, participant};
   }
 
   /**
    * Delete record
    */
-  async destroy({ params, response }: HttpContext) {
-    try {
-      const participant = await Participant.find(params.id)
-      if (!participant) {
-        return response.status(404).send({message: `Participant with ID ${params.id} was not found`})
-      }
-      await participant.delete()
-      return response.status(200).send({message: `Participant with ID ${params.id} was deleted`})
-    } catch (error) {
-      console.error(error)
-      return response.status(500).send({message: 'Internal server error'})
-    }
+  async destroy({ params }: HttpContext) {
+    const participant = await Participant.findOrFail(params.id);
+    await participant.delete();
+    return {"message": `Participant successfully deleted.`};
   }
 }
