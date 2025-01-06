abdaf22385b90f0366d466ef4d804a65be06aa9c
Kamil Ramocki <kamil.ramocki@gmail.com>
2025-01-06 10:50:21 +0100

    feat: add emails endpoints as in issue 44

--- app/validators/emails.ts
+++ app/validators/emails.ts
@@ -5,7 +5,7 @@ export const emailsStoreValidator = vine.compile(
     eventId: vine.number().optional(),
     name: vine.string(),
     content: vine.string(),
-    trigger: vine.enum(['manual', 'time', 'event']),
+    trigger: vine.enum(['participant_registered', 'form_filled', 'attribute_changed']),
     triggerValue: vine.string().optional(),
   })
 );
@@ -15,7 +15,7 @@ export const emailsUpdateValidator = vine.compile(
     eventId: vine.number().optional(),
     name: vine.string().optional(),
     content: vine.string().optional(),
-    trigger: vine.enum(['manual', 'time', 'event']).optional(),
+    trigger: vine.enum(['participant_registered', 'form_filled', 'attribute_changed']).optional(),
     triggerValue: vine.string().optional(),
   })
 );
