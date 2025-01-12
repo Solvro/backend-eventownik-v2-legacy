4979892c63014df28eff523fe31e47891e1031a9
Dawid Linek <dawid@linek.dev>
2025-01-12 19:30:03 +0100

    fix: non existing relation

--- app/models/participant.ts
+++ app/models/participant.ts
@@ -28,9 +28,6 @@ export default class Participant extends BaseModel {
   @column.dateTime({ autoCreate: true, autoUpdate: true })
   declare updatedAt: DateTime;
 
-  @hasMany(() => ParticipantAttribute)
-  declare attributes: HasMany<typeof ParticipantAttribute>
-
   @belongsTo(() => Event)
   declare event: BelongsTo<typeof Event>;
 
