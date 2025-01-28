c45916b9cceb8b478f88db236d2f0d53b832592f
Skiperpol <skiperpol1@gmail.com>
2025-01-28 13:19:21 +0100

    feat: add slug column to forms table

---
+++ database/migrations/1738063114364_add_slug_to_forms_table.ts
@@ -0,0 +1,17 @@
+import { BaseSchema } from '@adonisjs/lucid/schema'
+
+export default class AddSlugToForms extends BaseSchema {
+  protected tableName = 'forms';
+
+  async up() {
+    this.schema.alterTable(this.tableName, (table) => {
+      table.string('slug').notNullable().unique().after('name');
+    });
+  }
+
+  async down() {
+    this.schema.alterTable(this.tableName, (table) => {
+      table.dropColumn('slug');
+    });
+  }
+}
