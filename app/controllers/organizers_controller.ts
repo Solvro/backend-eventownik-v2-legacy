cd5203e618aeac7650fa2a8b3751f53a964d4e61
GOLDER303 <szymonstepien@interia.pl>
2025-02-09 13:22:36 +0100

    feat: add an endpoint that returns all organizers of an event

--- app/controllers/organizers_controller.ts
+++ app/controllers/organizers_controller.ts
@@ -1,1 +1,19 @@
-export default class OrganizersController {}
+import Admin from "#models/admin";
+import { HttpContext } from "@adonisjs/core/http";
+
+export default class OrganizersController {
+  /**
+   * @index
+   * @operationId getEventOrganizers
+   * @description Return an array of organizers of specified event
+   * @tag organizers
+   * @responseBody 200 - <Admin[]>
+   */
+  async index(context: HttpContext) {
+    const eventId = +context.params.eventId;
+
+    return await Admin.query()
+      .select("firstName", "lastName", "email")
+      .whereHas("events", (query) => query.where("events.id", eventId));
+  }
+}
