7269bda8bb75e8b91abec1c088196c06e4ab9955
Dawid Linek <dawid@linek.dev>
2025-02-19 22:14:55 +0100

    refactor: typecheck

--- app/validators/auth.ts
+++ app/validators/auth.ts
@@ -7,7 +7,6 @@ export const registerAdminValidator = vine.compile(
       .email()
       .unique(
         async (db, value) =>
-          // eslint-disable-next-line @typescript-eslint/strict-boolean-expressions
           !(await db.from("admins").where("email", value).first()),
       ),
     password: vine.string().minLength(8),
