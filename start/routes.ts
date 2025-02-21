782d4a4a81c80974635d971f9ae2c95f8f798f79
GOLDER303 <szymonstepien@interia.pl>
2025-02-21 14:30:45 +0100

    fix: fix routes

--- start/routes.ts
+++ start/routes.ts
@@ -6,15 +6,17 @@ import swagger from "#config/swagger";
 
 import { middleware } from "./kernel.js";
 
-// const BlocksController = () => import("#controllers/blocks_controller");
+const BlocksController = () => import("#controllers/blocks_controller");
 const EventController = () => import("#controllers/events_controller");
-// const ParticipantsController = () => import("#controllers/participants_controller");
+const ParticipantsController = () =>
+  import("#controllers/participants_controller");
 const AuthController = () => import("#controllers/auth_controller");
-// const PermissionsController = () => import("#controllers/permissions_controller");
-// const AdminsController = () => import("#controllers/admins_controller");
-// const OrganizersController = () => import("#controllers/organizers_controller");
+const PermissionsController = () =>
+  import("#controllers/permissions_controller");
+const AdminsController = () => import("#controllers/admins_controller");
+const OrganizersController = () => import("#controllers/organizers_controller");
 const FormsController = () => import("#controllers/forms_controller");
-//const EmailsController = () => import("#controllers/emails_controller");
+const EmailsController = () => import("#controllers/emails_controller");
 
 router.get("/swagger", async () => {
   return AutoSwagger.default.docs(router.toJSON(), swagger);
@@ -27,20 +29,19 @@ router
   .group(() => {
     router
       .group(() => {
+        router.resource("admins", AdminsController).apiOnly();
+        router.resource("events", EventController).apiOnly();
+        router.resource("permissions", PermissionsController).apiOnly();
+
         router
           .group(() => {
-            // router.resource("participants", ParticipantsController).apiOnly();
+            router.resource("blocks", BlocksController).apiOnly();
+            router.resource("emails", EmailsController).apiOnly();
             router.resource("forms", FormsController).apiOnly();
-
-            //router.resource("permissions", PermissionsController);
-            //router.resource("admins", AdminsController);
-            //router.resource("organizers", OrganizersController);
-            //router.resource("blocks", BlocksController);
-            //router.resource("emails", EmailsController);
+            router.resource("organizers", OrganizersController).apiOnly();
+            router.resource("participants", ParticipantsController).apiOnly();
           })
           .prefix("events/:eventId");
-
-        router.resource("events", EventController).apiOnly();
       })
       .use(middleware.auth());
 
