a21b83180170d859be4b578164fcbe6ed41f05d8
Dawid <dawid@linek.dev>
2025-03-15 21:30:55 +0100

    refactor: format

--- database/migrations/1741355167147_alter_default_show_in_table_attribute_true.ts
+++ database/migrations/1741355167147_alter_default_show_in_table_attribute_true.ts
@@ -4,10 +4,14 @@ export default class extends BaseSchema {
   protected tableName = "attributes";
 
   async up() {
-    this.schema.raw(`ALTER TABLE attributes ALTER COLUMN show_in_list SET DEFAULT TRUE;`);
+    this.schema.raw(
+      `ALTER TABLE attributes ALTER COLUMN show_in_list SET DEFAULT TRUE;`,
+    );
   }
 
   async down() {
-    this.schema.raw(`ALTER TABLE attributes ALTER COLUMN show_in_list SET DEFAULT FALSE;`);
+    this.schema.raw(
+      `ALTER TABLE attributes ALTER COLUMN show_in_list SET DEFAULT FALSE;`,
+    );
   }
 }
