479ea738c7fc33178538e4a98e3a0a92e755b27b
GOLDER303 <szymonstepien@interia.pl>
2025-03-20 19:00:58 +0100

    feat: add default file storage path

--- app/services/file_service.ts
+++ app/services/file_service.ts
@@ -3,12 +3,16 @@ import { randomUUID } from "node:crypto";
 import { MultipartFile } from "@adonisjs/core/bodyparser";
 import app from "@adonisjs/core/services/app";
 
+import env from "#start/env";
+
 export class FileService {
   async storeFile(
     file: MultipartFile,
-    fileStoragePath: string,
+    fileStoragePath?: string,
   ): Promise<string | undefined> {
-    await file.move(app.makePath(fileStoragePath), {
+    const filePath = fileStoragePath ?? env.get("FILE_STORAGE_URL", "uploads");
+
+    await file.move(app.makePath(filePath), {
       name: `${randomUUID()}.${file.extname}`,
     });
 
