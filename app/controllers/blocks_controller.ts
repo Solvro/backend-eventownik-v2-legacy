251676e2747830e275ac6fe0f5efa44b93d49953
ibtyog <piotrhirkyj@gmail.com>
2025-03-21 10:16:34 +0100

    docs: add blocks documentation

--- app/controllers/blocks_controller.ts
+++ app/controllers/blocks_controller.ts
@@ -4,12 +4,29 @@ import Block from "#models/block";
 import { createBlockValidator, updateBlockValidator } from "#validators/block";
 
 export default class BlocksController {
+  /**
+   * @index
+   * @operationId getBlocks
+   * @description Return a list of all blocks.
+   * @responseBody 200 - <Block[]>.paginated()
+   * @tag blocks
+   */
+
   async index({ request }: HttpContext) {
     return await Block.query().paginate(
       request.input("page", 1) | 1,
       request.input("perPage", 10) | 10,
     );
   }
+
+  /**
+   * @show
+   * @operationId showBlock
+   * @description Return a block with given ID.
+   * @responseBody 200 - <Block>
+   * @responseBody 404 - { "message": "Row not found", "name": "Exception", "status": 404 }
+   * @tag blocks
+   */
 
   async show({ params }: HttpContext) {
     const block = await Block.findOrFail(params.id);
@@ -20,12 +37,32 @@ export default class BlocksController {
     return block;
   }
 
+  /**
+   * @store
+   * @operationId storeBlock
+   * @summary Store a new block
+   * @description Store a new block. Note: parentId can be null.
+   * @requestBody <createBlockValidator>
+   * @responseBody 200 - <Block>
+   * @tag blocks
+   */
+
   async store({ request }: HttpContext) {
     const data = await createBlockValidator.validate(request.all());
     const block = await Block.create(data);
 
     return { message: "Block successfully created.", block };
   }
+
+  /**
+   * @update
+   * @operationId updateBlock
+   * @description Updates a block with given ID
+   * @requestBody <updateBlockValidator>
+   * @responseBody 200 - <Block>
+   * @responseBody 404 - { "message": "Row not found", "name": "Exception", "status": 404 }
+   * @tag blocks
+   */
 
   async update({ params, request }: HttpContext) {
     const data = await updateBlockValidator.validate(request.all());
@@ -35,6 +72,15 @@ export default class BlocksController {
 
     return { message: "Block successfully updated.", block };
   }
+
+  /**
+   * @destroy
+   * @operationId destroyBlock
+   * @description Destroys a block with given ID
+   * @responseBody 200 - { "message": "Block successfully deleted." }
+   * @responseBody 404 - { "message": "Row not found", "name": "Exception", "status": 404 }
+   * @tag blocks
+   */
 
   async destroy(ctx: HttpContext) {
     const block = await this.show(ctx);
