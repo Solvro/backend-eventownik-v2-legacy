6f6768126f877a89446c760ca893cbf74d3cf426
Dawid <dawid@linek.dev>
2025-03-21 23:58:57 +0100

    feat: return created at

--- app/controllers/participants_controller.ts
+++ app/controllers/participants_controller.ts
@@ -19,7 +19,7 @@ export default class ParticipantsController {
    * @tag participants
    * @summary Get all participants
    * @description Get all participants and their attributes for specific event
-   * @responseBody 200 - [{"id":32,"email":"test@test.pl","slug":"9081d217-9e13-4642-b7f0-2b8f8f409dfb","createdAt":"2025-02-19 13:56:10","updatedAt":"2025-02-19 13:56:10","attributes":[{"id":25,"name":"Sample Attribute","value":"sample value","slug":"sample-slug"}]}]
+   * @responseBody 200 - [{"id":32,"email":"test@test.pl", "created_at":"yyyy-MM-dd HH:mm:ss", "slug":"9081d217-9e13-4642-b7f0-2b8f8f409dfb","createdAt":"2025-02-19 13:56:10","updatedAt":"2025-02-19 13:56:10","attributes":[{"id":25,"name":"Sample Attribute","value":"sample value","slug":"sample-slug"}]}]
    */
   async index({ params }: HttpContext) {
     const participants = await Participant.query()
@@ -43,6 +43,7 @@ export default class ParticipantsController {
           slug: attribute.slug,
           value: attribute.$extras.pivot_value as string,
         })),
+        created_at: participant.createdAt.toFormat("yyyy-MM-dd HH:mm:ss"),
       };
     });
 
@@ -77,7 +78,7 @@ export default class ParticipantsController {
    * @tag participants
    * @summary Get a participant
    * @description Get a participant and sent emails for specific event
-   * @responseBody 200 - {"id": 1,"email":"john.doe@example.com","firstName": "John","lastName": "Doe","slug":"some-unique-slug","createdAt": "2025-02-18T00:56:06.115+01:00","updatedAt": "2025-02-18T00:56:06.115+01:00","emails":[{"id": 1,"name":"Welcome Email","content":"Welcome to our event!","participantEmails":{"status":"sent","sendBy": "admin","sendAt": "2025-02-19T14:43:12.000+01:00"}         }     ] }
+   * @responseBody 200 - {"id": 1,"email":"john.doe@example.com", "created_at":"yyyy-MM-dd HH:mm:ss", "firstName": "John","lastName": "Doe","slug":"some-unique-slug","createdAt": "2025-02-18T00:56:06.115+01:00","updatedAt": "2025-02-18T00:56:06.115+01:00","emails":[{"id": 1,"name":"Welcome Email","content":"Welcome to our event!","participantEmails":{"status":"sent","sendBy": "admin","sendAt": "2025-02-19T14:43:12.000+01:00"}         }     ] }
    * @responseBody 404 - { message: "Row not found", "name": "Exception", status: 404},
    */
   async show({ params, response }: HttpContext) {
@@ -110,6 +111,7 @@ export default class ParticipantsController {
       id: participant.id,
       email: participant.email,
       slug: participant.slug,
+      created_at: participant.createdAt.toFormat("yyyy-MM-dd HH:mm:ss"),
       attributes: participant.attributes.map((attribute) => ({
         id: attribute.id,
         name: attribute.name,
@@ -163,6 +165,7 @@ export default class ParticipantsController {
         slug: attribute.slug,
         value: attribute.$extras.pivot_value as string,
       })),
+      created_at: updatedParticipant.createdAt.toFormat("yyyy-MM-dd HH:mm:ss"),
     };
 
     return transformedUpdatedParticipant;
