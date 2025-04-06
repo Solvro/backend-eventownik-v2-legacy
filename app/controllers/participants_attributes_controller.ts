7bf446e41a9a2969e2fdbe9ef1ac6c4882706056
ibtyog <piotrhirkyj@gmail.com>
2025-04-06 14:39:43 +0200

    feat: add bulk attributes update

--- app/controllers/participants_attributes_controller.ts
+++ app/controllers/participants_attributes_controller.ts
@@ -1,10 +1,12 @@
 import { inject } from "@adonisjs/core";
 import type { HttpContext } from "@adonisjs/core/http";
+import db from "@adonisjs/lucid/services/db";
 
 import Attribute from "#models/attribute";
 import Event from "#models/event";
 import Participant from "#models/participant";
 import { FileService } from "#services/file_service";
+import { participantBulkUpdateValidator } from "#validators/participant_attributes";
 
 @inject()
 export default class ParticipantsAttributesController {
@@ -58,5 +60,66 @@ export default class ParticipantsAttributesController {
     } else {
       response.notFound({ message: "Attribute doesn't have a file" });
     }
+  }
+
+  async bulkUpdate({ params, request, bouncer, response }: HttpContext) {
+    const eventId = +params.eventId;
+    const payload = await request.validateUsing(participantBulkUpdateValidator);
+    const attributeId = +payload.attribute_id;
+    const newValue = payload.value;
+    const participantIds = payload.id;
+
+    await bouncer.authorize(
+      "manage_participant",
+      await Event.findOrFail(eventId),
+    );
+
+    const event = await Event.findOrFail(eventId);
+    await event
+      .related("attributes")
+      .query()
+      .where("id", attributeId)
+      .firstOrFail();
+    const foundParticipants = await event
+      .related("participants")
+      .query()
+      .whereIn("id", participantIds);
+
+    if (foundParticipants.length !== participantIds.length) {
+      const foundParticipantIds = foundParticipants.map(
+        (participant) => participant.id,
+      );
+      const missingIds = participantIds.filter(
+        (id) => !foundParticipantIds.includes(id),
+      );
+      return response.notFound(
+        `Participants with ID [${missingIds.join(",")}] not found.`,
+      );
+    }
+
+    const query = db
+      .from("participant_attributes")
+      .where("attribute_id", attributeId)
+      .andWhereIn("participant_id", participantIds);
+
+    const foundAttributes = await query.select("participant_id");
+
+    const foundParticipantIds = foundAttributes.map(
+      (attribute: { participant_id: number }) => attribute.participant_id,
+    );
+    const missingIds = participantIds.filter(
+      (id) => !foundParticipantIds.includes(id),
+    );
+    if (missingIds.length !== 0) {
+      return response.notFound(
+        `Attributes for participants with ID [${missingIds.join(",")}] not found.`,
+      );
+    }
+
+    await query.update({
+      value: newValue,
+    });
+
+    return response.noContent();
   }
 }
--- app/validators/participant_attributes.ts
+++ app/validators/participant_attributes.ts
@@ -1,27 +1,9 @@
 import vine from "@vinejs/vine";
 
-export const participantAttributesStoreValidator = vine.compile(
+export const participantBulkUpdateValidator = vine.compile(
   vine.object({
-    participantAttributes: vine
-      .array(
-        vine.object({
-          attributeId: vine.number(),
-          value: vine.string(),
-        }),
-      )
-      .optional(),
-  }),
-);
-
-export const participantAttributesUpdateValidator = vine.compile(
-  vine.object({
-    participantAttributes: vine
-      .array(
-        vine.object({
-          id: vine.number(),
-          value: vine.string(),
-        }),
-      )
-      .optional(),
+    id: vine.array(vine.number()),
+    attribute_id: vine.number(),
+    value: vine.string(),
   }),
 );
