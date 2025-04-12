d3bd9b6d069f7bd75df121dc89458dbfebf84a54
huskybtw <marekkocikw@gmail.com>
2025-04-12 21:29:43 +0200

    feat: duplicate mail

--- app/validators/emails.ts
+++ app/validators/emails.ts
@@ -47,3 +47,28 @@ export const emailsUpdateValidator = vine.compile(
       .requiredWhen("trigger", "=", "attribute_changed"),
   }),
 );
+
+export const emailDuplicateValidator = vine.compile(
+  vine.object({
+    name: vine.string(),
+    content: vine.string().optional(),
+    trigger: vine
+      .enum([
+        "participant_registered",
+        "participant_deleted",
+        "form_filled",
+        "attribute_changed",
+        "manual",
+      ])
+      .optional(),
+    triggerValue: vine
+      .string()
+      .optional()
+      .requiredWhen("trigger", "=", "form_filled")
+      .requiredWhen("trigger", "=", "attribute_changed"),
+    triggerValue2: vine
+      .string()
+      .optional()
+      .requiredWhen("trigger", "=", "attribute_changed"),
+  }),
+);
--- start/routes.ts
+++ start/routes.ts
@@ -68,6 +68,10 @@ router
             router.resource("blocks", BlocksController).apiOnly();
             router.resource("emails", EmailsController).apiOnly();
             router.post("emails/send/:emailId", [EmailsController, "send"]);
+            router.post("emails/duplicate/:emailId", [
+              EmailsController,
+              "duplicate",
+            ]);
             router.resource("forms", FormsController).apiOnly();
             router.resource("organizers", OrganizersController).apiOnly();
             // Participants/export and participants/import must be defined before the resource route
