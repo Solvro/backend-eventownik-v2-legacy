78ed3ce1d16f02c416fdae4e26dcbfced8a7e8fe
GOLDER303 <szymonstepien@interia.pl>
2025-04-13 14:40:07 +0200

    feat: include block participants in blocks show endpoint

--- app/services/block_service.ts
+++ app/services/block_service.ts
@@ -1,5 +1,6 @@
 import Attribute from "#models/attribute";
 import Block from "#models/block";
+import Participant from "#models/participant";
 
 export class BlockService {
   async getBlockTree(attributeId: number) {
@@ -23,5 +24,17 @@ export class BlockService {
     }
 
     return block;
+  }
+
+  async getBlockParticipants(attributeId: number, blockId: number) {
+    const participantsInBlock = await Participant.query().whereHas(
+      "attributes",
+      (query) => {
+        void query.where("attributes.id", attributeId);
+        void query.where("participant_attributes.value", "like", blockId);
+      },
+    );
+
+    return participantsInBlock;
   }
 }
