9da8a60f8ce48d5f1e84eebf3335aea3248f2a7b
K0smalka <284325@student.pwr.edu.pl>
2025-09-21 02:29:08 +0200

    fix: check the permissions to manage organizers

--- database/seeders/permission_seeder.ts
+++ database/seeders/permission_seeder.ts
@@ -13,6 +13,7 @@ export default class extends BaseSeeder {
         { action: "manage", subject: "form" },
         { action: "manage", subject: "participant" },
         { action: "manage", subject: "email" },
+        { action: "manage", subject: "organizer" },
       ],
     );
   }
