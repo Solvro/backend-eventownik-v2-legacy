d6406add6bc6ae44e81ed6046f7ae617d148fc93
GOLDER303 <szymonstepien@interia.pl>
2025-10-09 14:31:23 +0200

    feat: add password resets migration and model

---
+++ database/migrations/1760012499905_create_password_resets_table.ts
@@ -0,0 +1,23 @@
+import { BaseSchema } from "@adonisjs/lucid/schema";
+
+export default class extends BaseSchema {
+  protected tableName = "password_resets";
+
+  async up() {
+    this.schema.createTable(this.tableName, (table) => {
+      table.increments("id");
+
+      table.string("email").notNullable();
+      table.string("token").notNullable();
+      table.boolean("used").defaultTo(false);
+      table.timestamp("expiry_date").notNullable();
+
+      table.timestamp("created_at");
+      table.timestamp("updated_at");
+    });
+  }
+
+  async down() {
+    this.schema.dropTable(this.tableName);
+  }
+}
